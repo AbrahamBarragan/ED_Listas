a3b55b5263dee76fcfa50a6ced9c26a9d4da982f
Abraham Barragan Lira <bala.barragan3@hotmail.com>
2022-04-23 14:21:44 -0700

    Agregar ValidarVacio, Vaciar y RecorrerLista

--- ListasDoblementeLigadas/Listas.cs
+++ ListasDoblementeLigadas/Listas.cs
@@ -9,9 +9,32 @@ namespace ListasDoblementeLigadas
     internal class Listas
     {
         Nodo nodoInicial;
-        Nodo nodoAcutal;
+        Nodo nodoActual;
 
         public Listas()
+        {
+            nodoInicial = new Nodo();
+        }
+        public bool ValidarVacio()
+        {
+            return (nodoInicial.nodoSiguiente == null);
+        }
+        public void Vaciar()
+        {
+            nodoInicial.nodoSiguiente = null;
+        }
+        public String RecorrerLista()
+        {
+            string datosLista = "";
+            nodoActual = nodoInicial;
+            while (nodoActual.nodoSiguiente != null)
+            {
+                nodoActual = nodoActual.nodoSiguiente;
+                datosLista += $"{nodoActual.Valor}\n";
+            }
+            return datosLista;
+        }
+        public void AgregarNodo(string dato)
         {
 
         }
